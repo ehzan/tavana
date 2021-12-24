9d0818ce0b50112f7f20c8b36c5a6b9304c05f61
ehzan <enyazdi@gmail.com>
2021-12-24 17:29:42 +0330

    ver 1.0

--- website/urls.py
+++ website/urls.py
@@ -7,13 +7,14 @@ urlpatterns = [
 
     path('about/', views.about),
     path('coaches/', views.coaches),
+    path('rating-system/', views.page, name='rating-system'),
     path('contact/', views.contact),
 
     path('courses/', views.courses),
     path('free-session/', views.page, name='free-session'),
+    path('tutoring/', views.page, name='tutoring'),
     path('para-tabletennis/', views.page, name='para-tabletennis'),
     path('tournaments/', views.tournaments),
-    path('rating-system/', views.page, name='rating-system'),
 
     path('news/', views.news),
 
